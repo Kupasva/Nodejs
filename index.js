8a710413b781a51feaf12e2adf1bc73eba54d4ed
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-09-05 19:17:03 +0530

    kk

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express = require("express");
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send("hello world");
-    
-});
-
-app.listen(3000, () => {
-    console.log('server listening on port 3000');
-});
+console.log("kill me")
